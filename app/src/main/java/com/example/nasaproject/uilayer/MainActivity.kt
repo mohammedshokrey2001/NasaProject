fa01c832d1ca075cf6f4d33617664ca076ee1f29
mohamed shokry <mohamedhuter6@gmail.com>
2023-03-27 03:19:54 +0200

    DEPLOY 2

--- app/src/main/java/com/example/nasaproject/uilayer/MainActivity.kt
+++ app/src/main/java/com/example/nasaproject/uilayer/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.nasaproject.uilayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.nasaproject.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        Toast.makeText(this," Hello ",Toast.LENGTH_LONG).show()
     }
 }
